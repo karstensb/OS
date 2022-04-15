fc0afcc8c39806969001ad9585731eaf66cc9580
BOK2006 <benkarstens@web.de>
2022-04-15 13:03:33 +0200

    kernel enables interrupts after initializing

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,11 +3,13 @@
 #include "../cpu/idt.h"
 #include "../cpu/pic.h"
 #include "../cpu/tss.h"
+#include "../cpu/util.h"
 
 void kernel_main(void){
 	init_tss();
 	init_idt();
 	init_pic();
+	sti();
 	enable_cursor(CURSOR_MIN, CURSOR_MAX);
 	clear_screen();
 }
