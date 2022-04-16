e07d2c5fce798970dfe5c05ed7e9023c58b3e433
BOK2006 <benkarstens@web.de>
2022-04-16 20:15:11 +0200

    added outw, outl, inw, inl

--- cpu/util.h
+++ cpu/util.h
@@ -27,10 +27,33 @@ uint8_t inb(uint16_t port){
 	return result;
 }
 
+static inline
+uint16_t inw(uint16_t port){
+	uint16_t result;
+	asm volatile("in ax, dx" : "=a" (result) : "d" (port));
+	return result;
+}
+
+static inline
+uint32_t inl(uint16_t port){
+	uint32_t result;
+	asm volatile("in eax, dx" : "=a" (result) : "d" (port));
+	return result;
+}
 
 static inline
 void outb(uint16_t port, uint8_t data){
 	asm("out dx, al" : : "a" (data), "d" (port));
+}
+
+static inline
+void outw(uint16_t port, uint16_t data){
+	asm("out dx, ax" : : "a" (data), "d" (port));
+}
+
+static inline
+void outl(uint16_t port, uint32_t data){
+	asm("out dx, eax" : : "a" (data), "d" (port));
 }
 
 static inline
