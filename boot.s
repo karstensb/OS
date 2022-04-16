b8d5fe2829008ee8eee53b42995bbaffb020bc95
BOK2006 <benkarstens@web.de>
2022-04-16 20:15:56 +0200

    formatting

--- boot.s
+++ boot.s
@@ -1,17 +1,21 @@
+SECTION .multiboot
+
 ALIGNED equ 1<<0
 MEMINFO equ 1<<1
 FLAGS equ ALIGNED | MEMINFO
 MAGIC equ 0x1BADB002
 CHECKSUM equ -(MAGIC + FLAGS)
 
-SECTION .multiboot
 ALIGN 4
 dd MAGIC
 dd FLAGS
 dd CHECKSUM
 
-GLOBAL isr_stack_top
+;-------------------------------------------------------------------------------
 SECTION .bss
+
+GLOBAL isr_stack_top
+
 ALIGN 16
 stack_bottom:
 	resb 16384
@@ -21,13 +25,16 @@ isr_stack_bottom:
 	resb 16384
 isr_stack_top:
 
+;------------------------------------------------------------------------------
+SECTION .text
+
 KERNEL_CS equ 0x8
 KERNEL_DS equ 0x10
 
 EXTERN kernel_main
 EXTERN gdt_descriptor
-SECTION .text
 GLOBAL _start
+
 _start:
 	cli
 	mov ebp, stack_top
