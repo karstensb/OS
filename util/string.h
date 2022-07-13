ba2d304d97c0c4bb206277dbbd750c8b602d55cf
BOK2006 <benkarstens@web.de>
2022-07-13 15:35:42 +0200

    Add strcmp to header

--- util/string.h
+++ util/string.h
@@ -2,7 +2,7 @@
 
 size_t strlen(const char *str);
 char *strcat(char *dest, const char *src);
-
+int strcmp(const char *str1, const char *str2);
 void *memcpy(void *dest, const void * src, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
 void *memset(void *ptr, int x, size_t n);
