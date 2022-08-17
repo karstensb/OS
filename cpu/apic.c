beeda0c2e3e8a41be29e92c5afccd7135b207e7f
BOK2006 <benkarstens@web.de>
2022-08-17 19:58:41 +0200

    Don't mask LINT1 (legacy nmi)

--- cpu/apic.c
+++ cpu/apic.c
@@ -25,6 +25,11 @@ static void apic_spurious(UNUSED struct interrupt_frame *frame){
 	apic_eoi();
 }
 
+//TODO:
+void ioapic_init(void){
+	return;
+}
+
 void apic_init(void){
 	/* Note: the legacy 8259A PIC is not initialized (remmapped and masked)
 	 * because LINT0 on the APIC will be masked anyway, preventing any
@@ -33,12 +38,14 @@ void apic_init(void){
 	pg_map((void *) APIC_BASE, (void *) APIC_BASE, PG_PRESENT);
 	pg_used((void *) APIC_BASE);
 
+	ioapic_init();
+
 	/* mask all apic interrupts */
 	apic_write(APIC_LVT_TMR, APIC_LVT_MASKED);
 	apic_write(APIC_LVT_THERMAL, APIC_LVT_MASKED);
 	apic_write(APIC_LVT_PERF, APIC_LVT_MASKED);
 	apic_write(APIC_LVT_LINT0, APIC_LVT_MASKED);
-	apic_write(APIC_LVT_LINT1, APIC_LVT_MASKED);
+	/* leave LINT1 (legacy nmi) as configured by the BIOS */
 	apic_write(APIC_LVT_ERROR, APIC_LVT_MASKED);
 
 	/* set the spurious interrupt vector */
