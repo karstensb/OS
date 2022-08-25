7ae40534ffea40ea12fe6050092e081ea5936f0f
BOK2006 <benkarstens@web.de>
2022-08-25 20:15:40 +0200

    Use bitfields in idt_entry

--- cpu/idt.c
+++ cpu/idt.c
@@ -2,16 +2,15 @@
 #include "gdt.h"
 #include "x86.h"
 
-#define IDT_PRESENT (1 << 7)
-
 struct idt_entry idt[256];
 
-void idt_register_handler(uint8_t n, uint8_t type, uint32_t handler)
+void idt_register_handler(uint8_t n, enum idt_type type, uint32_t handler)
 {
 	idt[n].offset_low = handler & 0xFFFF;
 	idt[n].selector = KERNEL_CS;
-	idt[n].zero = 0;
-	idt[n].flags = IDT_PRESENT | type;
+	idt[n].type = type;
+	idt[n].dpl = 3;
+	idt[n].present = 1;
 	idt[n].offset_high = (handler >> 16) & 0xFFFF;
 	lidt(sizeof(idt) - 1, (uint32_t)&idt);
 }
--- cpu/idt.h
+++ cpu/idt.h
@@ -2,15 +2,24 @@
 
 #include <stdint.h>
 
-#define INTERRUPT_32 (0b1110) /* Automatically disable/reenable Interrupts */
-#define TRAP_32 (0b1111)
+enum idt_type
+{
+	TASK_GATE = 0x5,
+	INTERRUPT_16 = 0x6, /* Automatically disable/reenable Interrupts */
+	TRAP_16 = 0x7,
+	INTERRUPT_32 = 0xE, /* Automatically disable/reenable Interrupts */
+	TRAP_32 = 0xF,
+};
 
 struct idt_entry
 {
 	uint16_t offset_low;
 	uint16_t selector;
-	uint8_t zero;
-	uint8_t flags;
+	uint8_t:8;
+	uint8_t type : 4;
+	uint8_t:1;
+	uint8_t dpl : 2;
+	uint8_t present : 1;
 	uint16_t offset_high;
 } __attribute__((packed));
 
@@ -34,4 +43,4 @@ struct interrupt_frame
 
 extern struct idt_entry idt[256];
 
-void idt_register_handler(uint8_t n, uint8_t type, uint32_t handler);
+void idt_register_handler(uint8_t n, enum idt_type type, uint32_t handler);
